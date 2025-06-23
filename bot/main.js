9e4cfe118a3af346c86c74f0c80657e97017d090
Log1c0 <log1c05678@gmail.com>
2025-06-23 19:09:04 +0700

    fix: now bot token is obtained from env var

--- bot/main.js
+++ bot/main.js
@@ -2,5 +2,5 @@ const telgeramBot = require('./telegram/telegramBot')
 
 const clients = require('./clients/catalogClient')
 
-const bot = new telgeramBot.TelegramBot('7925301168:AAG_PYl1sZw6AGSDQ98jpR8MfIr9wJHYhxw');
+const bot = new telgeramBot.TelegramBot(process.env.BOT_TOKEN);
 bot.Start();
